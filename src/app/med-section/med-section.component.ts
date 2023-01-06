a01bb6af70cc985a2079ad3c3f9328a112b7297f
eperry26 <eperry1684@gmail.com>
2023-01-05 20:26:37 -0600

    able to remove meds

--- src/app/med-section/med-section.component.ts
+++ src/app/med-section/med-section.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, Input } from '@angular/core';
 import { Medication } from '../meds.interface';
 
 @Component({
@@ -6,29 +6,44 @@ import { Medication } from '../meds.interface';
   templateUrl: './med-section.component.html',
   styleUrls: ['./med-section.component.css']
 })
-export class MedSectionComponent {
+export class MedSectionComponent implements OnChanges, OnInit {
 
-  medList: Medication [] = [
-    {
-      medName: 'Prozac',
-      medDeliveryMethod: 'Pill',
-      dose: '10',
-      dosageUnits: 'mg',
-    },
-    {
-      medName: 'Metformin',
-      medDeliveryMethod: 'Pill',
-      dose: '850',
-      dosageUnits: 'mg',
-    },
-    {
-      medName: 'Albuterol',
-      medDeliveryMethod: 'Inhaler',
-      dose: '110',
-      dosageUnits: 'mcg'
-    },
-  ];
-  addMed(medName: string, medDeliveryMethod: string, dose: string , dosageUnits: string)  {// method to add a new medication
+
+  ngOnInit(): void {
+    this.medList = [
+      {
+        medName: 'Prozac',
+        medDeliveryMethod: 'Pill',
+        dose: '10',
+        dosageUnits: 'mg',
+      },
+      {
+        medName: 'Metformin',
+        medDeliveryMethod: 'Pill',
+        dose: '850',
+        dosageUnits: 'mg',
+      },
+      {
+        medName: 'Albuterol',
+        medDeliveryMethod: 'Inhaler',
+        dose: '110',
+        dosageUnits: 'mcg'
+      },
+    ];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("OnChanges called");
+  };
+
+  remBtnClicked: boolean = false;
+
+  removalChecks: Medication[] = [];
+
+  @Input() medList: Medication[] = [];
+
+
+  addMed(medName: string, medDeliveryMethod: string, dose: string, dosageUnits: string) {// method to add a new medication
     // use .push function
     const newMed = {
       medName,
@@ -36,8 +51,26 @@ export class MedSectionComponent {
       dose,
       dosageUnits,
     };
-    console.log('User entered: ',newMed);
+    //console.log('User entered: ',newMed);
     this.medList.push(newMed);
-     return this.medList;
-  }
+    return this.medList;
+  };
+
+  removeBtnClicked() { //go to different template
+    this.remBtnClicked = true;
+    return this.remBtnClicked;
+  };
+
+
+
+  removeMed(id: string) {
+    const index = this.medList.findIndex((obj) => { return obj.medName == id });
+    return this.medList.splice(index, 1);
+  };
+
+  backBtnClicked() {
+    this.remBtnClicked = false;
+    return this.remBtnClicked;
+  };
+
 }
